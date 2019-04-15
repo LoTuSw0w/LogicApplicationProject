a8c8271f897d7d8b8d936a79ed12db00cf6ec91a
Dung Luong <dung291196@gmail.com>
2019-04-15 10:54:40 +0200

    and function

---
+++ LogicApplication/LogicNotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicApplication
+{
+    class LogicNotation
+    {
+        //Converting function
+        public static string Conversion(string s)
+        {
+            //check if the proposition is an "and" proposition
+            if (s[0].Equals('&'))
+            {
+                return AndNotation.ReadAnd(s);
+            }
+            else
+            {
+                return "n0thing";
+            }
+        }
+    }
+}
